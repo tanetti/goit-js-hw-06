397454bd8f9b229497eea28f2be61c1c75dea07c
Volodymyr Dudka <tanetti22@gmail.com>
2022-07-13 02:10:53 +0300

    Add v2.0 solution to first task

--- js/task-01.js
+++ js/task-01.js
@@ -3,25 +3,43 @@ const categories = document.querySelector('#categories');
 console.log(`Number of categories: ${categories.children.length}`);
 console.log('\n');
 
-const categorySummary = parrentEl => {
+const findInnerCategories = parrentElement => {
   let countOfElements = 0;
 
-  for (const childrenElement of parrentEl.children) {
-    countOfElements += 1;
+  // -------------------- V 1.0 --------------------
 
-    console.group(`Element ${countOfElements}`);
+  // for (const childrenElement of parrentElement.children) {
+  //   countOfElements += 1;
+  //   console.group(`Element ${countOfElements}`);
 
-    for (const childrenInnerElement of childrenElement.children) {
-      if (childrenInnerElement.localName === 'h2') {
-        console.log(`Category: ${childrenInnerElement.innerHTML}`);
-      } else if (childrenInnerElement.localName === 'ul') {
-        console.log(`Elements: ${childrenInnerElement.children.length}`);
-      }
+  //   for (const innerChildrenElement of childrenElement.children) {
+  //     if (innerChildrenElement.localName === 'h2') {
+  //       console.log(`Category: ${innerChildrenElement.innerHTML}`);
+  //     } else if (innerChildrenElement.localName === 'ul') {
+  //       console.log(`Elements: ${innerChildrenElement.children.length}`);
+  //     }
+  //   }
+
+  //   console.groupEnd();
+  //   console.log('\n');
+  // }
+
+  // -------------------- V 2.0 --------------------
+
+  parrentElement.childNodes.forEach(childrenElement => {
+    if (childrenElement.localName === 'li') {
+      countOfElements += 1;
+      console.group(`Element ${countOfElements}`);
+
+      childrenElement.childNodes.forEach(innerChildrenElement => {
+        innerChildrenElement.localName === 'h2' && console.log(`Category: ${innerChildrenElement.innerHTML}`);
+        innerChildrenElement.localName === 'ul' && console.log(`Elements: ${innerChildrenElement.children.length}`);
+      });
+
+      console.groupEnd();
+      console.log('\n');
     }
-
-    console.groupEnd();
-    console.log('\n');
-  }
+  });
 };
 
-categorySummary(categories);
+findInnerCategories(categories);
