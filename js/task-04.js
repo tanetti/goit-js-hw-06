2d14bb640666d56bd68165c0dc1926cf955460eb
Volodymyr Dudka <tanetti22@gmail.com>
2022-07-13 14:21:52 +0300

    Add optimization for task four

--- js/task-04.js
+++ js/task-04.js
@@ -6,10 +6,10 @@ const refs = {
 let counterValue = 0;
 
 const incrementCouter = () => (refs.valueHandler.textContent = counterValue += 1);
-
 const decrementCouter = () => (refs.valueHandler.textContent = counterValue -= 1);
 
-refs.counterButtons.forEach(button => {
-  button.dataset.action === 'increment' && button.addEventListener('click', incrementCouter);
-  button.dataset.action === 'decrement' && button.addEventListener('click', decrementCouter);
-});
+refs.counterButtons.forEach(
+  button =>
+    (button.dataset.action === 'increment' && button.addEventListener('click', incrementCouter)) ||
+    (button.dataset.action === 'decrement' && button.addEventListener('click', decrementCouter))
+);
