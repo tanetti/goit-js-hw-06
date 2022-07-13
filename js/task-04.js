400ddf515e595f92cb97cada8216509cd330bda2
Volodymyr Dudka <tanetti22@gmail.com>
2022-07-13 14:17:43 +0300

    Add solution for task four

---
+++ js/task-04.js
@@ -0,0 +1,15 @@
+const refs = {
+  counterButtons: document.querySelectorAll('[data-action]'),
+  valueHandler: document.querySelector('span#value'),
+};
+
+let counterValue = 0;
+
+const incrementCouter = () => (refs.valueHandler.textContent = counterValue += 1);
+
+const decrementCouter = () => (refs.valueHandler.textContent = counterValue -= 1);
+
+refs.counterButtons.forEach(button => {
+  button.dataset.action === 'increment' && button.addEventListener('click', incrementCouter);
+  button.dataset.action === 'decrement' && button.addEventListener('click', decrementCouter);
+});
