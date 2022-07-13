71d9cc452fff53486b102da9e1dff18fa9f5f7c5
Volodymyr Dudka <tanetti22@gmail.com>
2022-07-13 16:07:28 +0300

    Add some optimization

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,6 @@
 const validationInputRef = document.querySelector('input#validation-input');
 
-const onValidationInputBlur = ({ currentTarget: input }) => {
+const onValidationInputBlur = ({ currentTarget: input }) =>
   input.value.length == input.dataset.length ? (input.classList.value = 'valid') : (input.classList.value = 'invalid');
-};
 
 validationInputRef.addEventListener('blur', onValidationInputBlur);
