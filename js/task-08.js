50e71281b216c70d39cd203713092c54d9625c96
Volodymyr Dudka <tanetti22@gmail.com>
2022-07-13 18:43:20 +0300

    Add solution for task eight

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const loginFormRef = document.querySelector('form.login-form');
 
+const onLoginFormSubmit = event => {
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
+
+  if (!email.value) {
+    alert('Please enter your Email address!');
+    return;
+  } else if (!password.value) {
+    alert('Please enter your Password!');
+    return;
+  }
+
+  const userCredentials = {
+    [email.name]: email.value,
+    [password.name]: password.value,
+  };
+
+  console.log(userCredentials);
+
+  event.currentTarget.reset();
+};
+
+loginFormRef.addEventListener('submit', onLoginFormSubmit);
