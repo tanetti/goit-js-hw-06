9dee65c3a800685268757ad65b3467c87acd5924
Volodymyr Dudka <tanetti22@gmail.com>
2022-07-13 19:01:26 +0300

    Add solution for task nine

--- js/task-09.js
+++ js/task-09.js
@@ -1,6 +1,13 @@
 const refs = {
   changeColorButton: document.querySelector('button.change-color'),
-  colorText: document.querySelector('span.color-text'),
+  colorText: document.querySelector('span.color'),
+  body: document.body,
 };
 
 const getRandomHexColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+const onChangeColorButtonClick = () => {
+  refs.colorText.textContent = refs.body.style.backgroundColor = getRandomHexColor();
+};
+
+refs.changeColorButton.addEventListener('click', onChangeColorButtonClick);
